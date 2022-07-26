18656ff62daab14e083ea9a632cd89c56a86980d
Jan Carlo De León <janleoncif11@gmail.com>
2022-07-25 19:05:35 -0600

    On branch main

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Prueba funcionamiento de Github</title>
+</head>
+<body>
+    <h1>Prueba de funcionamiento de GITHUB</h1>
+    <p>
+        <h2>
+            Haciendo despliegue en HEROKU
+        </h2>
+    </p>
+    <p>
+        <a href="funcionamiento.html">Ir a FUNCIONAMIENTO</a>
+    </p>
+</body>
+</html>
